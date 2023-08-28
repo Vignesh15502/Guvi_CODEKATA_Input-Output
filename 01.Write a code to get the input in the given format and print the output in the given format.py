a3e61fba1795ff80e5f1bb25fc7f6bd70809b90f
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-28 14:03:16 +0530

    Add files via upload

---
+++ 01.Write a code to get the input in the given format and print the output in the given format.py
@@ -0,0 +1,17 @@
+'''
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+To take an integer value
+
+Output Description:
+Print the integer value
+
+Sample Input :
+2
+Sample Output :
+2
+'''
+
+a=int(input())
+print(a)
