3766c29c24cd926fc1fdf5e662dd9a4f8005a315
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-28 14:03:41 +0530

    Add files via upload

---
+++ 02.Write a code to get the input in the given format and print the output in the given format.py
@@ -0,0 +1,17 @@
+'''
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+A single line contains integers separated by space
+
+Output Description:
+Print the integer list of integers separated by space
+
+Sample Input :
+2 3 4 5 6 7 8
+Sample Output :
+2 3 4 5 6 7 8
+'''
+
+a=input()
+print(a)
