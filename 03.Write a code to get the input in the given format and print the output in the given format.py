063bc0de146f69af912117a69d2021ec1869bedb
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-28 14:04:05 +0530

    Add files via upload

---
+++ 03.Write a code to get the input in the given format and print the output in the given format.py
@@ -0,0 +1,21 @@
+'''
+Write a code to get the input in the given format and print the output in the given format.
+
+Input Description:
+First-line indicates two integers which are the size of array and 'K' value. Second-line indicates an integer contains elements of an array.
+
+Output Description:
+Print the taken input in the same format.
+
+Sample Input :
+5 3
+1 2 3 4 5
+Sample Output :
+5 3
+1 2 3 4 5
+'''
+
+a=input()
+b=input()
+print(a)
+print(b)
