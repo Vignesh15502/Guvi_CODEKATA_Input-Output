2ff451b874c6eca3788afa559984c819da890d35
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-29 15:46:29 +0530

    Add files via upload

---
+++ 05.Write a code to get the input in the given format and print the output in the given format.py
@@ -0,0 +1,20 @@
+'''
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+Three integers are given in line by line.
+
+Output Description:
+Print the integers in a single line separate by space.
+
+Sample Input :
+2
+4
+5
+Sample Output :
+2 4 5
+'''
+a=input()
+b=input()
+c=input()
+print(a,b,c)
