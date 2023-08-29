3b388a124d15fe17cf01ef6cee29368eef7a8436
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-29 15:46:53 +0530

    Add files via upload

---
+++ 06.Write a code to get the input in the given format and print the output in the given format.py
@@ -0,0 +1,24 @@
+'''
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+First-line indicates two integers separated by space. Second-line indicates three integers separated by space. Third-line indicates three integers separated by space
+
+Output Description:
+Print the input in the same format.
+
+Sample Input :
+2 5
+2 5 6
+2 4 5
+Sample Output :
+2 5
+2 5 6
+2 4 5
+'''
+a=input()
+b=input()
+c=input()
+print(a)
+print(b)
+print(c)
