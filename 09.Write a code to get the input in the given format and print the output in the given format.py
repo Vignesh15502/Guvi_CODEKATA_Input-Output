7857647979f053c2a2b20367e9e94429bcdde107
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-01 03:07:59 +0530

    Add files via upload

---
+++ 09.Write a code to get the input in the given format and print the output in the given format.py
@@ -0,0 +1,24 @@
+'''
+Write a code to get the input in the given format and print the output in the given format.
+
+Input Description:
+A single line contains a string.
+
+Output Description:
+Print the characters in a string separated by line.
+
+Sample Input :
+guvigeek
+Sample Output :
+g
+u
+v
+i
+g
+e
+e
+k
+'''
+a=input()
+for i in a:
+    print(i)
